7fddd49014642061df77cb2464186e65fbe8c1ec
robert-walsh-hrri <rwalsh@gmail.com>
2020-11-30 15:28:49 -0600

    more working with the compiler

--- greeter.js
+++ greeter.js
@@ -1,16 +1,3 @@
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 var Student = /** @class */ (function () {
     function Student(firstName, middleInitial, lastName) {
         this.firstName = firstName;
@@ -30,49 +17,9 @@ function printLabel(labeledObj) {
 }
 var myObj = { size: 10, label: "Size 10 Object" };
 printLabel(myObj);
-var suits = ["hearts", "spades", "clubs", "diamonds"];
-function pickCard(x) {
-    // Check to see if we're working with an object/array
-    // if so, they gave us the deck and we'll pick the card
-    if (typeof x == "object") {
-        var pickedCard = Math.floor(Math.random() * x.length);
-        return pickedCard;
-    }
-    // Otherwise just let them pick the card
-    else if (typeof x == "number") {
-        var pickedSuit = Math.floor(x / 13);
-        return { suit: suits[pickedSuit], card: x % 13 };
-    }
+var someArray = [1, "string", false];
+for (var _i = 0, someArray_1 = someArray; _i < someArray_1.length; _i++) {
+    var entry = someArray_1[_i];
+    console.log(entry);
 }
-var myDeck = [
-    { suit: "diamonds", card: 2 },
-    { suit: "spades", card: 10 },
-    { suit: "hearts", card: 4 },
-];
-var pickedCard1 = myDeck[pickCard(myDeck)];
-alert("card: " + pickedCard1.card + " of " + pickedCard1.suit);
-var pickedCard2 = pickCard(15);
-alert("card: " + pickedCard2.card + " of " + pickedCard2.suit);
-var Animal = /** @class */ (function () {
-    function Animal() {
-    }
-    Animal.prototype.move = function (distanceInMeters) {
-        if (distanceInMeters === void 0) { distanceInMeters = 0; }
-        console.log("Animal moved " + distanceInMeters + "m.");
-    };
-    return Animal;
-}());
-var Dog = /** @class */ (function (_super) {
-    __extends(Dog, _super);
-    function Dog() {
-        return _super !== null && _super.apply(this, arguments) || this;
-    }
-    Dog.prototype.bark = function () {
-        console.log("Woof! Woof!");
-    };
-    return Dog;
-}(Animal));
-var dog = new Dog();
-dog.bark();
-dog.move(10);
-dog.bark();
+;
